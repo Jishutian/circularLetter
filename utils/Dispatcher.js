cb085758f2059a7ef883e872977617b6f121fd35
天南天北 <237840271@qq.com>
2019-03-29 14:09:40 +0800

    代码推送

---
+++ utils/Dispatcher.js
@@ -0,0 +1,33 @@
+var dispCbs = [];
+var dispIns = [];
+
+function Dispatcher(){
+	dispIns.push(this);
+  console.log(1111111,this)
+	dispCbs.push({});
+}
+Dispatcher.prototype = {
+	on(type, cb){
+		let cbtypes = dispCbs[dispIns.indexOf(this)];
+		let cbs = cbtypes[type] = (cbtypes[type] || []);
+		if(!~cbs.indexOf(cb)){
+			cbs.push(cb);
+		}
+	},
+	off(type, cb){
+		let cbtypes = dispCbs[dispIns.indexOf(this)];
+		let cbs = cbtypes[type] = (cbtypes[type] || []);
+		let curTypeCbIdx = cbs.indexOf(cb);
+		if(~curTypeCbIdx){
+			cbs.splice(curTypeCbIdx, 1);
+		}
+	},
+	fire(type, ...args){
+		let cbtypes = dispCbs[dispIns.indexOf(this)];
+		let cbs = cbtypes[type] = (cbtypes[type] || []);
+		for(let i = 0; i < cbs.length; i++){
+			cbs[i].apply(null, args);
+		}
+	}
+};
+module.exports = Dispatcher;
